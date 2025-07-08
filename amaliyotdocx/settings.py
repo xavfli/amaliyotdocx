bcb7eb7269cf583ad241c2c3b2be4a0ca6cc6845
Nuriddinov Boburbek(VO1D) <132753283+xavfli@users.noreply.github.com>
2025-07-08 18:06:18 +0500

    🔄 Static fayllar, CSS/JS, migrate va collectstatic qo‘shildi

--- amaliyotdocx/settings.py
+++ amaliyotdocx/settings.py
@@ -125,12 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app_excel/static')]
 
-STATICFILES_DIRS = [
-    BASE_DIR / "app_excel" / "static",
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
